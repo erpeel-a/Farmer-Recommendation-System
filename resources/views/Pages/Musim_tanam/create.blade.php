9c03e3d268c1ecf8a93480ae02807e899bb2e4ca
NFebri <nikofebri1480>
2020-10-06 20:24:12 +0700

    CRUD musim tanam

--- resources/views/Pages/Musim_tanam/create.blade.php
+++ resources/views/Pages/Musim_tanam/create.blade.php
@@ -6,7 +6,7 @@
           <div class="section-header">
               <h1>Tambah Musim Tanam</h1>
           </div>
-        <form action="#" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('musim.store') }}" method="POST" enctype="multipart/form-data">
           @csrf
             <div class="row">
                 <div class="col-md">
@@ -16,9 +16,9 @@
                             <label for="wilayah_id">Wilayah</label>
                             <select name="wilayah_id" id="wilayah_id" class="form-control @error('wilayah_id') is-invalid @enderror">
                               <option disabled selected>wilayah</option>
-                              <option value="">wilayah1</option>
-                              <option value="">wilayah2</option>
-                              <option value="">wilayah3</option>
+                              @foreach ($wilayah as $w)
+                                <option value="{{ $w->id }}">{{ $w->nama_wilayah }}</option>
+                              @endforeach
                             </select>
                             @error('wilayah_id')
                               <div class="invalid-feedback">
@@ -31,9 +31,9 @@
                               <label for="rekomendasi_id">Rekomendasi</label>
                               <select name="rekomendasi_id" id="rekomendasi_id" class="form-control @error('rekomendasi_id') is-invalid @enderror">
                                 <option disabled selected>rekomendasi</option>
-                                <option value="">rekomendasi1</option>
-                                <option value="">rekomendasi2</option>
-                                <option value="">rekomendasi3</option>
+                                @foreach ($rekomendasi as $r)
+                                  <option value="{{ $r->id }}">{{ $r->kebutuhan->nama_kebutuhan }}</option>
+                                @endforeach
                               </select>
                               @error('rekomendasi_id')
                                 <div class="invalid-feedback">
@@ -44,7 +44,7 @@
 
                             <div class="form-group">
                               <label for="nomor_musim">Nomor Musim</label>
-                              <input type="number" id="nomor_musim" class="form-control @error('nomor_musim') is-invalid @enderror" name="nomor_musim" autofocus="" value="{{old('nomor_musim')}}">
+                              <input type="number" id="nomor_musim" class="form-control @error('nomor_musim') is-invalid @enderror" name="nomor_musim"  value="{{old('nomor_musim')}}">
                               @error('nomor_musim')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -54,7 +54,7 @@
 
                             <div class="form-group">
                               <label for="nama_petani">Nama Petani</label>
-                              <input type="text" id="nama_petani" class="form-control @error('nama_petani') is-invalid @enderror" name="nama_petani" autofocus="" value="{{old('nama_petani')}}">
+                              <input type="text" id="nama_petani" class="form-control @error('nama_petani') is-invalid @enderror" name="nama_petani"  value="{{old('nama_petani')}}">
                               @error('nama_petani')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -64,7 +64,7 @@
 
                             <div class="form-group">
                               <label for="nama_musim">Nama Musim</label>
-                              <input type="text" id="nama_musim" class="form-control @error('nama_musim') is-invalid @enderror" name="nama_musim" autofocus="" value="{{old('nama_musim')}}">
+                              <input type="text" id="nama_musim" class="form-control @error('nama_musim') is-invalid @enderror" name="nama_musim"  value="{{old('nama_musim')}}">
                               @error('nama_musim')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -74,7 +74,7 @@
 
                             <div class="form-group">
                               <label for="luas_tanah">Luas Tanah</label>
-                              <input type="number" id="luas_tanah" class="form-control @error('luas_tanah') is-invalid @enderror" name="luas_tanah" autofocus="" value="{{old('luas_tanah')}}">
+                              <input type="number" id="luas_tanah" class="form-control @error('luas_tanah') is-invalid @enderror" name="luas_tanah"  value="{{old('luas_tanah')}}">
                               @error('luas_tanah')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -84,7 +84,7 @@
 
                             <div class="form-group">
                               <label for="tanggal_tanam">Tanggal Tanam</label>
-                              <input type="date" id="tanggal_tanam" class="form-control @error('tanggal_tanam') is-invalid @enderror" name="tanggal_tanam" autofocus="" value="{{old('tanggal_tanam')}}">
+                              <input type="date" id="tanggal_tanam" class="form-control @error('tanggal_tanam') is-invalid @enderror" name="tanggal_tanam"  value="{{old('tanggal_tanam')}}">
                               @error('tanggal_tanam')
                                 <div class="invalid-feedback">
                                   {{$message}}
--- resources/views/Pages/Musim_tanam/edit.blade.php
+++ resources/views/Pages/Musim_tanam/edit.blade.php
@@ -6,8 +6,9 @@
           <div class="section-header">
               <h1>Edit Musim Tanam</h1>
           </div>
-        <form action="#" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('musim.update', $musim->id) }}" method="POST" enctype="multipart/form-data">
           @csrf
+          @method('put')
             <div class="row">
                 <div class="col-md">
                     <div class="card">
@@ -16,10 +17,9 @@
                           <div class="form-group">
                             <label for="wilayah_id">Wilayah</label>
                             <select name="wilayah_id" id="wilayah_id" class="form-control @error('wilayah_id') is-invalid @enderror">
-                              <option disabled selected>wilayah</option>
-                              <option value="">wilayah1</option>
-                              <option value="">wilayah2</option>
-                              <option value="">wilayah3</option>
+                              @foreach ($wilayah as $w)
+                              <option {{ $w->id == $musim->wilayah_id ? ' selected' : '' }} value="{{ $w->id }}">{{ $w->nama_wilayah }}</option>
+                              @endforeach
                             </select>
                             @error('wilayah_id')
                               <div class="invalid-feedback">
@@ -31,10 +31,9 @@
                             <div class="form-group">
                               <label for="rekomendasi_id">Rekomendasi</label>
                               <select name="rekomendasi_id" id="rekomendasi_id" class="form-control @error('rekomendasi_id') is-invalid @enderror">
-                                <option disabled selected>rekomendasi</option>
-                                <option value="">rekomendasi1</option>
-                                <option value="">rekomendasi2</option>
-                                <option value="">rekomendasi3</option>
+                                @foreach ($rekomendasi as $r)
+                                <option {{ $r->id == $musim->rekomendasi_id ? ' selected' : '' }} value="{{ $r->id }}">{{ $r->kebutuhan->nama_kebutuhan }}</option>
+                                @endforeach
                               </select>
                               @error('rekomendasi_id')
                                 <div class="invalid-feedback">
@@ -45,7 +44,7 @@
 
                             <div class="form-group">
                               <label for="nomor_musim">Nomor Musim</label>
-                              <input type="number" id="nomor_musim" class="form-control @error('nomor_musim') is-invalid @enderror" name="nomor_musim" autofocus="" value="{{old('nomor_musim')}}">
+                              <input type="number" id="nomor_musim" class="form-control @error('nomor_musim') is-invalid @enderror" name="nomor_musim"  value="{{ $musim->nomor_musim }}">
                               @error('nomor_musim')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -55,7 +54,7 @@
 
                             <div class="form-group">
                               <label for="nama_petani">Nama Petani</label>
-                              <input type="text" id="nama_petani" class="form-control @error('nama_petani') is-invalid @enderror" name="nama_petani" autofocus="" value="{{old('nama_petani')}}">
+                              <input type="text" id="nama_petani" class="form-control @error('nama_petani') is-invalid @enderror" name="nama_petani"  value="{{ $musim->nama_petani }}">
                               @error('nama_petani')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -65,7 +64,7 @@
 
                             <div class="form-group">
                               <label for="nama_musim">Nama Musim</label>
-                              <input type="text" id="nama_musim" class="form-control @error('nama_musim') is-invalid @enderror" name="nama_musim" autofocus="" value="{{old('nama_musim')}}">
+                              <input type="text" id="nama_musim" class="form-control @error('nama_musim') is-invalid @enderror" name="nama_musim"  value="{{ $musim->nama_musim }}">
                               @error('nama_musim')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -75,7 +74,7 @@
 
                             <div class="form-group">
                               <label for="luas_tanah">Luas Tanah</label>
-                              <input type="number" id="luas_tanah" class="form-control @error('luas_tanah') is-invalid @enderror" name="luas_tanah" autofocus="" value="{{old('luas_tanah')}}">
+                              <input type="number" id="luas_tanah" class="form-control @error('luas_tanah') is-invalid @enderror" name="luas_tanah"  value="{{ $musim->luas_tanah }}">
                               @error('luas_tanah')
                                 <div class="invalid-feedback">
                                   {{$message}}
@@ -85,7 +84,7 @@
 
                             <div class="form-group">
                               <label for="tanggal_tanam">Tanggal Tanam</label>
-                              <input type="date" id="tanggal_tanam" class="form-control @error('tanggal_tanam') is-invalid @enderror" name="tanggal_tanam" autofocus="" value="{{old('tanggal_tanam')}}">
+                              <input type="date" id="tanggal_tanam" class="form-control @error('tanggal_tanam') is-invalid @enderror" name="tanggal_tanam"  value="{{ $musim->tanggal_tanam }}">
                               @error('tanggal_tanam')
                                 <div class="invalid-feedback">
                                   {{$message}}
