b593a188536d577cfe176eec5e8bbf204ab097f4
muhammadafin <muhammadafin787@gmail.com>
2020-10-11 23:48:11 +0700

    fix bug

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -39,7 +39,7 @@
         <div class="container d-flex justify-content-between align-items-center">
          <div class="konten">
             <h1 class="display-4">We help you find<br>a suitable recomendation<br>for farmers.</h1>
-            <p class="lead">An amazing platform to help you choose<br>the best farmers recomendations..</p>
+            <p class="lead">An amazing platform to help you choose<br>the best farmers recomendations.</p>
             <button type="button" class="btn btn-try rounded-0" href="">Try Now</a>
             <button type="button" class="btn btn-about rounded-0" href="">About App</a>
          </div>
@@ -203,6 +203,6 @@
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
   </body>
 </html>
